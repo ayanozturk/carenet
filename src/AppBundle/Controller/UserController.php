733af421bf7fa218a158b72a1bed140017631a06
Ayan Ozturk <ayanozturk@gmail.com>
2017-08-13 17:08:51 +0100

    - add 404 page - add profile edit buttons - handle public profiles

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,10 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller
 {
-    public function profileAction()
+    public function profileAction($user_id)
     {
+        $userService = $this->container->get('AppBundle\Service\User');
+        $user = $userService->findById($user_id);
+
+        if (!$user) {
+            return $this->render('default/404.html.twig');
+        }
+
         return $this->render('user/profile.html.twig', [
-            'user' => $this->getUser()
+            'user' => $user,
+            'myprofile' => $user === $this->getUser()
         ]);
     }
 }
